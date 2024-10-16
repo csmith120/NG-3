5a707b7eb689a1d7cb1b538d6b16dda43a630444
csmith120 <taskmaster3654@gmail.com>
2024-10-16 12:23:46 -0500

    making emmiter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-user',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+export class UserComponent {
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
+
+  onSelectUser() {}
 }
